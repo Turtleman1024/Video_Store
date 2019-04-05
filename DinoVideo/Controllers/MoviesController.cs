4be22203f1bff988a7731e5fd2bddc9f234fa118
Turtleman1024 <kmurry6262@gmail.com>
2019-04-04 20:59:24 -0700

    Example of parameter actions and overriding

--- DinoVideo/Controllers/MoviesController.cs
+++ DinoVideo/Controllers/MoviesController.cs
@@ -35,5 +35,27 @@ namespace DinoVideo.Controllers
         {
             return Content("id=" + id);
         }
+
+        /// <summary>
+        /// Example of parameter Action
+        /// Could return a view from the database
+        /// GOTO:
+        /// /movies
+        /// Can Override by doing
+        /// /movies?pageIndex=2&sortBy=ReleaseDate
+        /// </summary>
+        /// <param name="pageIndex">Is optional</param>
+        /// <param name="sortby"></param>
+        /// <returns>Could return a list of movies from DB</returns>
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
     }
 }
