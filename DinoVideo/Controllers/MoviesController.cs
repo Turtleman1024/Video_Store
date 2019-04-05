86b037a464f1ffbbfb78c28e6149c8d0191e6e45
Turtleman1024 <kmurry6262@gmail.com>
2019-04-04 20:43:45 -0700

    Action Parameter Example

--- DinoVideo/Controllers/MoviesController.cs
+++ DinoVideo/Controllers/MoviesController.cs
@@ -13,15 +13,27 @@ namespace DinoVideo.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Jurassic Park" };
-
-            //return View(movie);
             //Below are some example of Action Results
             //return Content("Hello World!");
             //return HttpNotFound();
             //return new EmptyResult();
             //If you look in the address bar it will show page=1&sortby=name
-            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name"});
+            //return RedirectToAction("Index", "Home", new { page = 1, sortby = "name"});
 
+            return View(movie);
+
+        }
+
+        /// <summary>
+        /// Example of Action Parameters
+        /// /movies/edit/1
+        /// or 
+        /// Query String example
+        /// /movies/edit?id=1
+        /// </summary>
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
         }
     }
 }
