a07e8f8c569af85a873dd5ba616c5f47858009cc
Turtleman1024 <kmurry6262@gmail.com>
2019-04-06 20:21:45 -0700

    Example of a custom route

--- DinoVideo/App_Start/RouteConfig.cs
+++ DinoVideo/App_Start/RouteConfig.cs
@@ -13,6 +13,18 @@ namespace DinoVideo
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Example of a custom route with three parameters
+            //Most go before default as the order matters
+            //Define most specific to most generic
+            //"/movies/released/2015/04"
+            routes.MapRoute(
+                //Route Name should be unique
+                "MoviesByReleaseDate",
+                //URL Pattern
+                "movies/released/{year}/{month}",
+                //Use a generic anonymous object
+                new { controller = "Movies", action = "ByReleaseDate"});
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
